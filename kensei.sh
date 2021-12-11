4e733cebd1ab238c828243e5798372e5d5817b4d
John Doe <kencoding1031@gmail.com>
2021-12-11 15:00:16 +0900

    sh_file

---
+++ kensei.sh
@@ -0,0 +1,3 @@
+mvn install
+cp target/x-earth-1.0-SNAPSHOT.jar ../mc_server/spigot-1.17.1/plugins/x-earth-1.0-SNAPSHOT.jar
+echo "all process success"
---
+++ kensei_run.sh
@@ -0,0 +1,2 @@
+cd /Users/saryusoul/desktop/mc_server/spigot-1.17.1
+java -jar spigot-1.17.1.jar
